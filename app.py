b0cc0ee15a3e740eaab130f5a833eae176366112
MykhailoB-P <mykhailobudonnyi@gmail.com>
2025-08-13 23:52:32 +0100

    function for adding expenses data to the .json file

--- app.py
+++ app.py
@@ -6,6 +6,15 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+DATA_FILE = 'expenses.json'
+
+# Function for adding expenses
+def load_expenses():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    return []
+
 @app.route('/')
 def hello_world():
     return 'Hello world, my backend is working now!'
