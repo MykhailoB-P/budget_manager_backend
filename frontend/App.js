57e56e5f7d7314d25f7731464472b9c8e862fefe
MykhailoB-P <mykhailobudonnyi@gmail.com>
2025-08-14 22:47:08 +0100

    app.js has been contained

---
+++ frontend/App.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import Expenses from './Expenses';
+import AddExpense from './AddExpense';
+
+function App() {
+  const [expenses, setExpenses] = useState([]);
+
+  useEffect(() => {
+    fetch('http://127.0.0.1:5000/expenses')
+      .then(res => res.json())
+      .then(data => setExpenses(data));
+  }, []);
+
+  const addExpense = (expense) => {
+    setExpenses([...expenses, expense]);
+  };
+
+  return (
+    <div>
+      <h1>My First Web App - Budget Manager</h1>
+      <AddExpense addExpense={addExpense} />
+      <Expenses expenses={expenses} />
+    </div>
+  );
+}
+
+export default App;
