634c267db931b583a8a7cec8c8410664afe60bde
MykhailoB-P <mykhailobudonnyi@gmail.com>
2025-08-16 22:06:21 +0100

    function for Expenses

--- frontend/Expenses.js
+++ frontend/Expenses.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+function Expenses({ expenses }) {
+    return (
+      <div>
+        <h2>Expenses List</h2>
+        <ul>
+          {expenses.map((e, index) => (
+            <li key={index}>
+              {e.category}: ${e.amount} on {e.date}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  
+  export default Expenses;
