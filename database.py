fd028c9d75a226f51fa125a93f89aa4a5e3b5ad3
MykhailoB-P <mykhailobudonnyi@gmail.com>
2025-08-16 23:29:20 +0100

    database corrections

--- database.py
+++ database.py
@@ -1,25 +1,10 @@
 from pymongo import MongoClient
-from datetime import datetime
 
-# Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")  # default local MongoDB
-db = client["budget_manager"]                        # database name
-expenses_collection = db["expenses"]                 # collection name
+# Connect to local MongoDB
+client = MongoClient("mongodb://localhost:27017/")
 
-# Add a new expense
-def add_expense(expense):
-    if "date" not in expense:
-        expense["date"] = datetime.now()
-    return expenses_collection.insert_one(expense)
+# Create/use database
+db = client["budget_manager"]
 
-# Get all expenses
-def get_all_expenses():
-    return list(expenses_collection.find({}, {"_id": 0}))  # hide _id for cleaner JSON
-
-# Get stats by category
-def get_expense_stats():
-    stats = {}
-    for exp in get_all_expenses():
-        category = exp["category"]
-        stats[category] = stats.get(category, 0) + exp["amount"]
-    return stats
+# Create/use collection
+expenses_collection = db["expenses"]
